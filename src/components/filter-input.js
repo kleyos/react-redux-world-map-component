868775eeae934a85bf272fa05e84bd749f0cc84e
Olga Erushenko <olga.erushenko@gmail.com>
2017-07-27 19:55:14 +0300

    add tooltips label on hover marker

--- src/components/filter-input.js
+++ src/components/filter-input.js
@@ -1,17 +1,11 @@
 import React, { Component } from 'react'
 
 class FilterInput extends Component{
-	handlePressEnter(e){
-	console.log('write value', this.filterInput.value)
-	//  if (e.key==='Enter') {
-	// 		 console.log('write value', this.filterInput.value)
-	// 	 }
- }
 	render(){
 		const { filtering } = this.props
 
-		return <div>
-			<label className=""> POPULATION </label>
+		return <div className="filter">
+			<label className="filter-label"> POPULATION </label>
 			<input className="filter-input" type="number"
 				ref={input => this.filterInput=input}
 				onBlur={()=>filtering(this.filterInput.value)}/>
--- src/components/world-map.js
+++ src/components/world-map.js
@@ -1,28 +1,16 @@
 import React, { Component } from 'react'
-
 import { geoMercator, geoPath } from "d3-geo"
-import { feature } from "topojson-client"
 
 class WorldMap extends Component {
 
 	projection() {
-		return geoMercator().scale(100).translate([ 1000/2, 800/2 ])
+		return geoMercator().scale(100).translate([ 1000/2, 450/2 ])
 	}
-	componentDidMount() {
-		const { fetchDataMap, fetchDataPop } = this.props
 
-		fetch("https://raw.githubusercontent.com/d3/d3.github.com/master/world-110m.v1.json")
-			.then(response => response.json())
-			.then(worldData => fetchDataMap( feature(worldData, worldData.objects.countries).features ))
-
-		fetch("https://raw.githubusercontent.com/kleyos/react-redux-world-map-component/master/src/utils/population.json")
-				.then(response => response.json())
-				.then(population => fetchDataPop(population))
-	}
 	render() {
-		const { worldData=[], population=[]} = this.props
+		const { worldData=[], population=[], filtValue } = this.props
 		return (
-			<svg width={`${100}%`} height={`${100}%`} viewBox="0 0 1000 800">
+			<svg width={`${100}%`} height={`${100}%`} viewBox="0 0 1000 450">
 				<g className="countries">
 					{
 						worldData.map((d,i) => (
@@ -39,16 +27,23 @@ class WorldMap extends Component {
 				</g>
 				<g className="markers">
 					{population.length>0
-						? population.filter(item => item.Population > 500000)
+						? population.filter(item => item.Population > filtValue)
 							.map( (city,i) =>
-								<circle
-									key={ `marker-${i}` }
-									cx={ this.projection()([city.Long, city.Lat])[0]}
-									cy={ this.projection()([city.Long, city.Lat])[1]}
-									r={ city.Population / 3000000 }
-									fill="#E91E63"
-									stroke="#FFFFFF"
-									className="marker"/>)
+								<g key={i} className="marker-container">
+									<circle
+										key={ `marker-${i}` }
+										cx={ this.projection()([city.Long, city.Lat])[0]}
+										cy={ this.projection()([city.Long, city.Lat])[1]}
+										r={ city.Population / 2000000 }
+										fill={'rgba(233,30,99,255)'}
+										className="marker"/>
+									<text className="marker-label"
+										key={ `marker-label-${i}` }
+										x={ this.projection()([city.Long, city.Lat])[0] }
+										y={ this.projection()([city.Long, city.Lat])[1]+2*(city.Population / 2000000) }>
+										{`${city['City Name']}, population: ${city['Population']}`}
+									</text>
+								</g>)
 						: false
 					}
 				</g>
