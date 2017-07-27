d69eb8abbbbb854b4d9a537be27dab12f595169a
Olga Erushenko <olga.erushenko@gmail.com>
2017-07-27 16:45:29 +0300

    add default state population 500000

--- src/store.js
+++ src/store.js
@@ -2,14 +2,9 @@ import { createStore, applyMiddleware } from 'redux'
 import rootReducer from './reducers/index'
 import thunk from 'redux-thunk'
 
-//const middlewares = [].push(thunk);
+const defaultState = { filtValue:500000 }
 
-// if (process.env.NODE_ENV === `development`) {
-//   const { logger } = require(`redux-logger`);
-//   middlewares.push(logger);
-// }
-
-const store = createStore(rootReducer, applyMiddleware(thunk))
+const store = createStore(rootReducer, defaultState, applyMiddleware(thunk))
 
 if (module.hot) {
 		module.hot.accept('./reducers', () => {
