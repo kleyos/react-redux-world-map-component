59eefa994f5643c533380c0b8b5be4d959259d37
Olga Erushenko <olga.erushenko@gmail.com>
2017-07-28 10:53:50 +0300

    add section with video

---
+++ src/components/work.js
@@ -0,0 +1,25 @@
+import React from 'react'
+//import logo from '../styles/logo.svg'
+
+const Work = ()=>{
+	// const navItems=['How we work', 'Companies', 'Testimonials', 'Contact Us']
+	// const navBtns=['Sign Up', 'Login']
+	return <section className="section work">
+		<h1>HOW WE WORK</h1>
+		<div className="container-fluid">
+			<div className="row">
+				<div className="col col-md-6">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+				<div className="col col-md-6">
+					<video width="480" controls>
+					  <source
+					    src="https://photos.app.goo.gl/t3c1Tzfdb279VcWA2"
+					    type="video/webm" />
+					  	Your browser doesn't support HTML5 video tag.
+					</video>
+			</div>
+			</div>
+		</div>
+	</section>
+	}
+
+	export default Work
